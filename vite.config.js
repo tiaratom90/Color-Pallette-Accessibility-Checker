7f224009b8f569df4992857ea413353892bb7403
tiaratom90 <thomas.dalbec@gmail.com>
2025-06-18 19:06:04 -0400

    Fix package.json syntax and update deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-export default defineConfig({
-  base: '/Color-Pallette-Accessibility-Checker/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/Color-Pallette-Accessibility-Checker/',
   server: {
-    host: "::",
+    host: true,
     port: 8080,
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
+
